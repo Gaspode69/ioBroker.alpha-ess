9fff659506c0f4ad6f21dc3aa6fc739db3e5efb1
Gaspode69 <45949835+Gaspode69@users.noreply.github.com>
2024-11-27 17:01:24 +0100

    Enable jsdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,15 +15,16 @@ export default [
             'admin/build',
             'admin/words.js',
             'admin/admin.d.ts',
-            '**/adapter-config.d.ts',
-        ],
+            '**/adapter-config.d.ts'
+        ]
     },
 
     {
         // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
         // as this improves maintainability. jsdoc warnings will not block buiuld process.
         rules: {
-            'jsdoc/require-jsdoc': 'off',
+            // 'jsdoc/require-jsdoc': 'off',
         },
     },
+
 ];
